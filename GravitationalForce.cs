49a6c9ea539e487de5119e1a622c359433c80845
Erica Slotkin <77819164+ferris4936@users.noreply.github.com>
2021-07-03 03:00:29 -0600

    Add project files.

---
+++ GravitationalForce.cs
@@ -0,0 +1,39 @@
+﻿//Programmer: Erica Slotkin
+
+//Filename: GravitationalForce
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Slotkin_Q1
+{
+    class GravitationalForce
+    {
+        public const double G = 667408000000;
+        public double F { get; private set; } //gravitational force in newtons
+        public double M1 { get; set; } //mass of object 1 in kilograms
+        public double M2 { get; set; } //mass of object 2 in kilograms
+        public double R { get; set; } //distance between M1 and M2 in meters 
+
+        public GravitationalForce()
+        {
+
+        }
+
+        public GravitationalForce(double mass1, double mass2, double distance)
+        {
+            this.M1 = mass1;
+            this.M2 = mass2;
+            this.R = distance;
+        }
+
+        public double CalcGravForce()
+        {
+            this.F = G*(M1 * M2 / R * R);  
+            return F; 
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿//Programmer: Erica Slotkin
+
+//Filename: Program.cs
+
+using System;
+
+namespace Slotkin_Q1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string answer = "y";
+            do
+            {
+                Console.WriteLine("\nThis program will calculate the gravitational force between 2 objects given");
+                Console.WriteLine("the mass of each object and the distance between the two.  Let's do this!");
+
+                Console.WriteLine("\nPlease enter the mass of the first object in kilograms: ");
+                string massObject1 = Console.ReadLine();
+                double mass1 = Convert.ToDouble(massObject1);     
+
+                Console.WriteLine("\nPlease enter the mass of the second object in kilograms: ");
+                string massObject2 = Console.ReadLine();
+                double mass2 = Convert.ToDouble(massObject2);
+
+                Console.WriteLine("\nPlease enter the distance between the 2 objects in meters: ");
+                string distance = Console.ReadLine();
+                double dist = Convert.ToDouble(distance);
+
+                GravitationalForce force = new GravitationalForce(mass1, mass2, dist);
+
+                string results = force.CalcGravForce().ToString();
+
+                Console.WriteLine("\nThe gravitational force between the two objects is {0} newton.  Impressed?", results); 
+
+                Console.WriteLine("\nWould you like to do another? y/n");
+                answer = Console.ReadLine();
+
+            } while (answer == "y");
+
+            Console.WriteLine("\nHope I passed this quiz!");
+        }
+    }
+}
